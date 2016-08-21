8de5e4dfbe6edfcdda8723e9104325cc3c1b3536
VVeb Developer <developer@vveb.pro>
2016-08-21 14:16:58 +0300

    Added license information

--- src/Synchronizer/Console/Application.php
+++ src/Synchronizer/Console/Application.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ *  This file is part of the Magento Project Synchronizer.
+ *
+ *  (c) Magero team <support@magero.pw>
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ */
 
 namespace Magero\Project\Synchronizer\Console;
 
